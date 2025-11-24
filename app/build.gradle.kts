b7a45cf2db2d2c28cce5a1fcd1ef87e337a86ff9
Muhammad <mohamedkam000@gmail.com>
2025-11-24 06:43:21 +0000

    ooof

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,4 +58,16 @@ android {
 
 dependencies {
     implementation("androidx.appcompat:appcompat:1.7.1")
+    implementation(platform("androidx.compose:compose-bom:2025.11.00"))
+    implementation("androidx.compose.ui:ui:1.10.0-beta01")
+    implementation("androidx.compose.animation:animation:1.9.0-rc01")
+    implementation("androidx.compose.ui:ui-tooling:1.10.0-alpha01")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.10.0-beta01")
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.activity:activity-compose:1.12.0-alpha06")
+    implementation("androidx.compose.material:material-icons-core:1.7.8")
+    implementation("androidx.compose.material:material-icons-extended:1.7.8")
+    implementation("androidx.compose.runtime:runtime:1.10.0-alpha01")
+    implementation("androidx.compose.material3:material3:1.5.0-alpha02")
+    implementation("androidx.compose.foundation:foundation:1.10.0-beta01")
 }
